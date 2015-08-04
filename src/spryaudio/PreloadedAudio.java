fbe27ea1cfdcca5fba0bd2cd1c7c225ee4eaaae4
Christian Holton <cmholton@gmail.com>
2015-08-04 00:44:46 -0700

    Added javadoc comments to PreloadedAudio and StreamingAudio.

--- src/spryaudio/PreloadedAudio.java
+++ src/spryaudio/PreloadedAudio.java
@@ -3,24 +3,78 @@ package spryaudio;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 
+/**
+ * {@code Audio} that loads it audio data completely into memory prior to being
+ * played. {@code PreloadedAudio} can be compared to {@code StreamingAudio}
+ * using the following attributes:
+ * <p>
+ * <ul>
+ * <li>Functionality - Compared to {@code StreamingAudio},
+ * {@code PreloadedAudio} offers extra functionality. Because
+ * {@code PreloadedAudio} loads all its audio data into memory, the duration of
+ * the audio can be determined (via the {@code getLength} method) and playback
+ * can occur at an arbitrary position (via the {@code setPosition} method).</li>
+ * <li>Memory - Compared to {@code PreloadedAudio}, {@code StreamingAudio} uses
+ * less memory. {@code PreloadedAudio} requires memory equal to the size of its
+ * audio file, while {@code StreamingAudio} only needs enough memory for its
+ * read buffer.</li>
+ * <li>Latency - {@code PreloadedAudio} will generally have less latency than
+ * {@code StreamingAudio}. {@code PreloadedAudio} has all of its audio data in
+ * memory and can therefore start playback immediately after a call to one of
+ * the {@code play} methods, while {@code StreamingAudio} will need to wait for
+ * its read buffer to be filled. In practice, any difference in latency is
+ * rarely noticeable.</li>
+ * </ul>
+ * 
+ * @author Christian Holton
+ * 
+ * @see PreloadedPlayback
+ *
+ */
 public class PreloadedAudio extends Audio {
-	
 
+	/**
+	 * The size of the input buffer that will temporarily hold the data read
+	 * from the {@code AudioInputStream}.
+	 */
 	private static final int BUFFER_SIZE = 4096;
-
+	/**
+	 * An input stream with a specified audio format and length. The length is
+	 * expressed in sample frames, not bytes.
+	 */
 	private AudioInputStream audioInStream;
-
+	/**
+	 * Specifies the particular arrangement of data in audioInStream.
+	 */
 	private AudioFormat audioFormat;
-
+	/**
+	 * An array of converted bytes from a {@code ByteArrayOutputStream}. This
+	 * will hold the data that will be used by a {@code PreloadedPlayback} at
+	 * each call to the {@code startPlayback} method.
+	 */
 	private byte[] audioBytes;
-
+	/**
+	 * When one of the {@code play} methods is called, there will be a small
+	 * amount of latency that occurs before the playback of audio actually
+	 * begins. In an effort to minimize this latency, a
+	 * {@code PreloadedPlayback} object is "prepped" by creating it ahead of
+	 * time, before it is needed, and saving it for the next call to a
+	 * {@code play} method.
+	 */
 	private PreloadedPlayback nextPlay;
-	
 
+	/**
+	 * Creates a new {@code PreloadedAudio} that is based on the audio file with
+	 * the specified {@code URL}.
+	 *
+	 * @param fileURL
+	 *            The audio file's {@code URL}.
+	 */
 	protected PreloadedAudio(URL fileURL) {
 		this.fileURL = fileURL;
 		audioInStream = SpryAudio.acquireAudioInputStream(fileURL);
@@ -49,10 +103,22 @@ public class PreloadedAudio extends Audio {
 		return startPlayback(volume, numLoops);
 	}
 
+	/**
+	 * Set the position that this {@code PreloadedAudio} will begin playback at
+	 * once one of the {@code play} methods are called.
+	 * 
+	 * @param seconds
+	 *            The desired position in seconds.
+	 */
 	public void setPosition(double seconds) {
 		nextPlay.setPosition(seconds);
 	}
 
+	/**
+	 * Get the length of this {@code PreloadedAudio} in seconds.
+	 * 
+	 * @return The length in seconds.
+	 */
 	public double getLength() {
 		return nextPlay.getLength();
 	}
@@ -62,6 +128,13 @@ public class PreloadedAudio extends Audio {
 		return "PreloadedAudio " + getFileName();
 	}
 
+	/**
+	 * Read the audio data from the {@code AudioInputStream} in chunks and write
+	 * it to a {@code ByteArrayOutputStream} which will then be converted into a
+	 * byte array.
+	 *
+	 * @return Success
+	 */
 	private boolean loadDataAndPrep() {
 		ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
 		byte[] inBuffer = new byte[BUFFER_SIZE
@@ -94,6 +167,19 @@ public class PreloadedAudio extends Audio {
 		return true;
 	}
 
+	/**
+	 * Call the current {@code PreloadedPlayback} to begin playback and create a
+	 * {@code PreloadedPlayback} for the next playback. This method is called by
+	 * all the {@code play} methods.
+	 * 
+	 * @param volume
+	 *            The volume that playback will occur at.
+	 * @param numLoops
+	 *            The number of times the audio data will be played in
+	 *            succession.
+	 * 
+	 * @return The {@code PreloadedPlayback} which is to be played.
+	 */
 	private PreloadedPlayback startPlayback(double volume, int numLoops) {
 		PreloadedPlayback currentPlay = nextPlay;
 		currentPlay.start(volume, numLoops, exec);
--- src/spryaudio/StreamingAudio.java
+++ src/spryaudio/StreamingAudio.java
@@ -2,14 +2,57 @@ package spryaudio;
 
 import java.net.URL;
 
+/**
+ * {@code Audio} that streams its audio data, i.e. it is played while
+ * simultaneously read into memory. {@code StreamingAudio} can be compared to
+ * {@code PreloadedAudio} using the following attributes:
+ * <p>
+ * <ul>
+ * <li>Functionality - Compared to {@code StreamingAudio},
+ * {@code PreloadedAudio} offers extra functionality. Because
+ * {@code PreloadedAudio} loads all its audio data into memory, the duration of
+ * the audio can be determined (via the {@code getLength} method) and playback
+ * can occur at an arbitrary position (via the {@code setPosition} method).</li>
+ * <li>Memory - Compared to {@code PreloadedAudio}, {@code StreamingAudio} uses
+ * less memory. {@code PreloadedAudio} requires memory equal to the size of its
+ * audio file, while {@code StreamingAudio} only needs enough memory for its
+ * read buffer.</li>
+ * <li>Latency - {@code PreloadedAudio} will generally have less latency than
+ * {@code StreamingAudio}. {@code PreloadedAudio} has all of its audio data in
+ * memory and can therefore start playback immediately after a call to one of
+ * the {@code play} methods, while {@code StreamingAudio} will need to wait for
+ * its read buffer to be filled. In practice, any difference in latency is
+ * rarely noticeable.</li>
+ * </ul>
+ * 
+ * @author Christian Holton
+ * 
+ * @see StreamingPlayback
+ *
+ */
 public class StreamingAudio extends Audio {
 
+	/**
+	 * When one of the {@code play} methods is called, there will be a small
+	 * amount of latency that occurs before the playback of audio actually
+	 * begins. In an effort to minimize this latency, a
+	 * {@code StreamingPlayback} object is "prepped" by creating it ahead of
+	 * time, before it is needed, and saving it for the next call to a
+	 * {@code play} method.
+	 */
 	private StreamingPlayback nextPlay;
 
+	/**
+	 * Creates a new {@code StreamingAudio} that is based on the audio file with
+	 * the specified {@code URL}.
+	 *
+	 * @param fileURL
+	 *            The audio file's {@code URL}.
+	 */
 	protected StreamingAudio(URL fileURL) {
 		this.fileURL = fileURL;
 		nextPlay = new StreamingPlayback(this,
-				SpryAudio.acquireAudioInputStream(fileURL), numPlaybacks++);
+				SpryAudio.acquireAudioInputStream(fileURL), ++numPlaybacks);
 	}
 
 	@Override
@@ -38,13 +81,26 @@ public class StreamingAudio extends Audio {
 		return "StreamingAudio " + getFileName();
 	}
 
+	/**
+	 * Call the current {@code StreamingPlayback} to begin playback and create a
+	 * {@code StreamingPlayback} for the next playback. This method is called by
+	 * all the {@code play()} methods.
+	 *
+	 * @param volume
+	 *            The volume that playback will occur at.
+	 * @param numLoops
+	 *            The number of times the audio data will be played in
+	 *            succession.
+	 * 
+	 * @return The {@code StreamingPlayback} which is to be played.
+	 */
 	private StreamingPlayback startPlayback(double volume, int numLoops) {
 		StreamingPlayback currentPlayback = nextPlay;
 		currentPlayback.start(volume, numLoops, exec);
 
 		// "Prep" for the next call to one of the play methods.
 		nextPlay = new StreamingPlayback(this,
-				SpryAudio.acquireAudioInputStream(fileURL), numPlaybacks++);
+				SpryAudio.acquireAudioInputStream(fileURL), ++numPlaybacks);
 		return currentPlayback;
 	}
 }
